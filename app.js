b6d26ecfc66a63422df705903fb6fe0ed9d13c0f
JordanCollier <jordanicollier@gmail.com>
2015-05-18 11:37:55 -0600

    add high and low toggle

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
 	console.log("The document is ready!");
 
+
+// Background buttons
 	$(".dark").on("click", function(){
 		$('body').css('background', '#000 url(img/dark_wood.png) repeat');
 	});
@@ -14,7 +16,61 @@ $(document).ready(function() {
 		$('body').css('background-size', "cover");
 	});
 
+// Low and High Toggle Buttons
 
+// Low Toggle
+var lowToggle = function(){
+   $('.low').on('click', function(e){
+     e.preventDefault();
+
+     var myArray = $("article");
+     var count = 0;
+
+     myArray.sort(function (a, b) {
+       a = parseInt($(a).attr("data-price"), 10);
+       b = parseInt($(b).attr("data-price"), 10);
+       count += 2;
+
+       if(a > b) {
+           return 1;
+       } else if(a < b) {
+           return -1;
+       } else {
+           return 0;
+       }
+     });
+     $(".image-container").append(myArray);
+   });
+ };
+
+    lowToggle();
+
+// High Toggle
+		var highToggle = function(){
+		   $('.high').on('click', function(e){
+		     e.preventDefault();
+
+		     var myArray = $("article");
+		     var count = 0;
+
+		     myArray.sort(function (a, b) {
+		       a = parseInt($(a).attr("data-price"), 10);
+		       b = parseInt($(b).attr("data-price"), 10);
+		       count += 2;
+
+		       if(a > b) {
+		           return -1;
+		       } else if(a < b) {
+		           return 1;
+		       } else {
+		           return 0;
+		       }
+		     });
+		     $(".image-container").append(myArray);
+		   });
+		 };
+
+		highToggle();
 
 
 });
