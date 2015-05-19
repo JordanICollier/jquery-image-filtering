e69730fba026590cc311caf47f3108063fcafc2a
JordanCollier <jordanicollier@gmail.com>
2015-05-19 00:24:31 -0600

    can sort by high and low using lodash

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
 
 	var cabins = [
-		{src: 'one', display_price: "$20,000", price: "20000"},
-		{src: 'two', display_price: "$30,000", price: "30000"},
-		{src: 'three', display_price: "$40,000", price: "40000"},
-		{src: 'four', display_price: "$1,000,000", price: "1000000"},
-		{src: 'five', display_price: "$250,000", price: "250000"},
-		{src: 'six', display_price: "$90,000", price: "90000"},
-		{src: 'seven', display_price: "$750,000", price: "750000"},
-		{src: 'eight', display_price: "$60,000", price: "60000"},
-		{src: 'nine', display_price: "$100,000", price: "100000"},
-		{src: 'ten', display_price: "$750,000", price: "750000"},
-		{src: 'eleven', display_price: "$60,000", price: "60000"},
-		{src: 'twelve', display_price: "$1,000", price: "1000"},
-		{src: 'syrup', display_price: "$2", price: "2"},
-		{src: 'lincoln', display_price: "$20", price: "20"},
-		{src: 'old_cabin', display_price: "$1,000", price: "1000"}
+		{src: 'one', display_price: "$20,000", price: 20000},
+		{src: 'two', display_price: "$30,000", price: 30000},
+		{src: 'three', display_price: "$40,000", price: 40000},
+		{src: 'four', display_price: "$1,000,000", price: 1000000},
+		{src: 'five', display_price: "$250,000", price: 250000},
+		{src: 'six', display_price: "$90,000", price: 90000},
+		{src: 'seven', display_price: "$750,000", price: 750000},
+		{src: 'eight', display_price: "$60,000", price: 60000},
+		{src: 'nine', display_price: "$100,000", price: 100000},
+		{src: 'ten', display_price: "$750,000", price: 750000},
+		{src: 'eleven', display_price: "$60,000", price: 60000},
+		{src: 'twelve', display_price: "$1,000", price: 1000},
+		{src: 'syrup', display_price: "$2", price: 2},
+		{src: 'lincoln', display_price: "$20", price: 20},
+		{src: 'old_cabin', display_price: "$1,000", price: 1000}
 	];
 
 	// Handlebars
@@ -25,9 +25,6 @@ $(document).ready(function() {
 
 	$('.image-container').html(html);
 	console.log(html);
-
-
-	console.log("The document is ready!");
 
 
 // Background buttons
@@ -43,72 +40,20 @@ $(document).ready(function() {
 		$('body').css('background', '#000 url(img/forest.jpg) repeat');
 		$('body').css('background-size', "cover");
 	});
-
-// Low and High Toggle Buttons
-
-// Low Toggle
-var lowToggle = function(){
-   $('.low').on('click', function(e){
-     e.preventDefault();
-
-     var myArray = $("article");
-     var count = 0;
-
-     myArray.sort(function (a, b) {
-       a = parseInt($(a).attr("data-price"), 10);
-       b = parseInt($(b).attr("data-price"), 10);
-       count += 2;
-
-       if(a > b) {
-           return 1;
-       } else if(a < b) {
-           return -1;
-       } else {
-           return 0;
-       }
-     });
-     $(".image-container").append(myArray);
-   });
- };
-
-    lowToggle();
-
-// High Toggle
-		var highToggle = function(){
-		   $('.high').on('click', function(e){
-		     e.preventDefault();
-
-		     var myArray = $("article");
-		     var count = 0;
-
-		     myArray.sort(function (a, b) {
-		       a = parseInt($(a).attr("data-price"), 10);
-		       b = parseInt($(b).attr("data-price"), 10);
-		       count += 2;
-
-		       if(a > b) {
-		           return -1;
-		       } else if(a < b) {
-		           return 1;
-		       } else {
-		           return 0;
-		       }
-		     });
-		     $(".image-container").append(myArray);
-		   });
-		 };
-
-		highToggle();
-
-// Showing One Price Range
-
-
-	var sortCabin = function(){ $('input[type="checkbox"][value="50000"]').on('click', function(){
-		console.log("yes");
-
-		});
-	};
-	sortCabin();
-
+// Sort by high and low
+	// Sort Low
+	$('.low').on("click", function(e){
+		e.preventDefault();
+		var lowSorted = _.sortBy(cabins, 'price');
+		var html = template({cabins: lowSorted});
+		$('.image-container').html(html);
+	});
+	// Sort High
+	$('.high').on("click", function(e){
+		e.preventDefault();
+		var highSorted = _.sortBy(cabins, 'price').reverse();
+		var html = template({cabins: highSorted});
+		$('.image-container').html(html);
+	});
 
 });
